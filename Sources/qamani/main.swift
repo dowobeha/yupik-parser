0de2981e9f9db2fd55681a3b263093b4055fd328
Lane Schwartz <dowobeha@gmail.com>
2020-05-29 09:14:02 -0500

    Refactor

--- Sources/qamani/main.swift
+++ Sources/qamani/main.swift
@@ -1,18 +1,1 @@
-import ArgumentParser
-import Foma
-
-struct RunFST: ParsableCommand {
-    
-    @Option(default: "/Users/lanes/work/summer/yupik/yupik-foma-v2/lower.fomabin",
-            help:    "Finite-state transducer in foma binary file format")
-    var fst: String
-    
-    func run() {
-        let fst = FST(fromBinary: self.fst) //FST(fromBinary: "/Users/lanes/work/summer/yupik/yupik-foma-v2/lower.fomabin")
-
-        let result = fst.applyUp("qikmiq")
-        print("Hello, \(result)!")
-    }
-}
-
-RunFST.main()
+Parser.main()
