f2e45e77bf343078fb2e9fb9729ba020719bb8ad
Lane Schwartz <dowobeha@gmail.com>
2020-05-29 09:05:29 -0500

    Added argument parser

--- Package.swift
+++ Package.swift
@@ -7,18 +7,17 @@ let package = Package(
     name: "qamani",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        Package.Dependency.package(url: "https://github.com/dowobeha/Foma.git", from: "0.0.4"),
+        Package.Dependency.package(url: "https://github.com/dowobeha/Foma.git", from: "0.1.1"),
+        Package.Dependency.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         Target.target(
             name: "qamani",
-            dependencies: [],
-            linkerSettings: [LinkerSetting.unsafeFlags(["-Xlinker", "-L/usr/local/lib"])]),
+            dependencies: ["Foma", "ArgumentParser"]),
         Target.testTarget(
             name: "qamaniTests",
-            dependencies: ["qamani"],
-            linkerSettings: [LinkerSetting.unsafeFlags(["-Xlinker", "-L/usr/local/lib"])])
+            dependencies: ["Foma", "ArgumentParser", "qamani"]),
     ]
 )
