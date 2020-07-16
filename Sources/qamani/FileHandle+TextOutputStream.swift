faab3c3382b180a06922bf977f288e0ab38bf6bb
Lane Schwartz <dowobeha@gmail.com>
2020-07-16 13:09:54 -0500

    Remove commented out code

--- Sources/qamani/FileHandle+TextOutputStream.swift
+++ Sources/qamani/FileHandle+TextOutputStream.swift
@@ -1,12 +1,4 @@
-//public struct StandardErrorOutputStream: TextOutputStream {
-//    public mutating func write(_ string: String) { fputs(string, stderr) }
-//}
-//
-//public var stderr = StandardErrorOutputStream()
-
 import Foundation
-
-// var stderr = FileHandle.standardError
 
 extension FileHandle : TextOutputStream {
   public func write(_ string: String) {
