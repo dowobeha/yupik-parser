ae73531bf50207a83c6843088881fda93b264226
Lane Schwartz <dowobeha@gmail.com>
2020-07-16 11:41:23 -0500

    Support 3 modes: all, unique, and failure

--- Sources/qamani/Parser.swift
+++ Sources/qamani/Parser.swift
@@ -14,14 +14,21 @@ struct Parser: ParsableCommand {
     @Option(help:    "Text file containing one sentence per line")
     var sentences: String
 
-    @Option(help:    "Mode: failure")
-    var mode: String
+    enum Mode: String, ExpressibleByArgument { case all, unique, failure }
+    @Option(help:    """
+                     Mode: all     (Print count and value of all analyzes for every word)
+                           unique  (Print count and value of analyses for words with exactly 1 analysis)
+                           failure (Print words that failed to analyze)
+
+                     """)
+    var mode: Mode = Mode.all
     
     func run() {
+
         guard let l2s = FST(fromBinary: self.l2s) else {
             print("Unable to open \(self.l2s)", to: &stderr)
             return
-        } //FST(fromBinary: "/Users/lanes/work/summer/yupik/yupik-foma-v2/lower.fomabin")
+        }
 
         guard let l2i = FST(fromBinary: self.l2is) else {
             print("Unable to open \(self.l2is)", to: &stderr)
@@ -33,34 +40,34 @@ struct Parser: ParsableCommand {
             return
         }
 
-        if self.mode == "failures" {
-            for sentence in parsedSentences {
-                //print(sentence.words.count)
+        for sentence in parsedSentences {
+
+            let paths: Int = sentence.words.reduce(1, { (r:Int, w:AnalyzedWord) -> Int in return r * w.analyses.count})
+            for word in sentence {
+                let forms: String = word.analyses.map{ $0.underlyingForm }.joined(separator: "\t")
+                var actual: String = "FAILURE"
+                if let a: String = word.actualSurfaceForm {
+                    actual = a
+                }
                 
-                //var seen = Set<String>()
-                let paths: Int = sentence.words.reduce(1, { (r:Int, w:AnalyzedWord) -> Int in return r * w.analyses.count})
-                for word in sentence {
-                    let forms: String = word.analyses.map{ $0.underlyingForm }.joined(separator: "\t")
-                    var actual: String = "FAILURE"
-                    if let a: String = word.actualSurfaceForm {
-                        actual = a
-                    }
-                    if word.analyses.count >= 0 { //  && !seen.contains(actual)
-                        //seen.insert(actual)
+                switch self.mode {
+                case Mode.all:
+                    print("\(word.analyses.count)\t\(actual)\t\(paths)\tWord \(word.wordNumber) of sentence \(sentence.lineNumber) in document \(sentence.document)\t\(word.originalSurfaceForm)\t\(forms)")
+                case Mode.unique:
+                    if word.analyses.count == 1 {
                         print("\(word.analyses.count)\t\(actual)\t\(paths)\tWord \(word.wordNumber) of sentence \(sentence.lineNumber) in document \(sentence.document)\t\(word.originalSurfaceForm)\t\(forms)")
                     }
-                    /*
+                case Mode.failure:
                     if word.analyses.isEmpty {
-                        print("0\t\(word.originalSurfaceForm)\tWord \(word.wordNumber) of sentence \(sentence.lineNumber) in document \(sentence.document)")
-                    } else {
-                        print("SUCCESS\t\(word.analyses.count)\t\(word.originalSurfaceForm)\tWord \(word.wordNumber) of sentence \(sentence.lineNumber) in document \(sentence.document)")
+                        print("\(word.analyses.count)\t\(actual)\t\(paths)\tWord \(word.wordNumber) of sentence \(sentence.lineNumber) in document \(sentence.document)\t\(word.originalSurfaceForm)\t\(forms)")
                     }
- */
                 }
             }
         }
+    
     }
     
+    /// Read sentences from the provided file and morphologically analyze every word in every sentence in the file.
     static func parseFile(_ filename: String, using l2s: FST, and l2i: FST) -> [AnalyzedSentence]? {
         if let lines = StreamReader(path: filename) {
             var document = filename
