6dafb6dc0807acab224c8171be88571f784b41ad
Lane Schwartz <dowobeha@gmail.com>
2020-07-17 13:59:19 -0500

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -5,20 +5,46 @@ import PackageDescription
 
 let package = Package(
     name: "qamani",
+    products: [
+        Product.executable(
+            name: "qamani",
+            targets: ["qamani"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        Package.Dependency.package(url: "https://github.com/dowobeha/Foma.git", from: "0.3.1"),
-        Package.Dependency.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.2.0"),
-        Package.Dependency.package(url: "https://github.com/hectr/swift-stream-reader.git", from: "0.3.0")
+        Package.Dependency.package(
+            name: "Foma",
+            url: "https://github.com/dowobeha/Foma.git",
+            from: "0.3.1"),
+        Package.Dependency.package(
+            name: "swift-argument-parser",
+            url: "https://github.com/apple/swift-argument-parser.git",
+            from: "0.2.0"),
+        Package.Dependency.package(
+            name: "StreamReader",
+            url: "https://github.com/hectr/swift-stream-reader.git",
+            from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         Target.target(
             name: "qamani",
-            dependencies: ["Foma", "ArgumentParser", "StreamReader"]),
+            dependencies: [
+                Target.Dependency.product(
+                    name: "Foma",
+                    package: "Foma"),
+                Target.Dependency.product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"),
+                Target.Dependency.product(
+                    name: "StreamReader",
+                    package: "StreamReader")
+            ]),
         Target.testTarget(
             name: "qamaniTests",
-            dependencies: ["qamani"]),
+            dependencies: [
+                Target.Dependency.target(name: "qamani")
+            ]),
     ]
 )
