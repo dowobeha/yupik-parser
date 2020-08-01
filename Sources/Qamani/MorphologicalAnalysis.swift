7f4943fec21a95231019a80af1c51f5eccd6875c
Lane Schwartz <dowobeha@gmail.com>
2020-08-01 07:51:58 -0500

    Code cleanup

--- Sources/Qamani/MorphologicalAnalysis.swift
+++ Sources/Qamani/MorphologicalAnalysis.swift
@@ -2,10 +2,7 @@ import Foma
 
 /// Morphological analysis of a single word
 public struct MorphologicalAnalysis: Codable {
-    
-    public static let startOfWord = "<w>"
-    public static let endOfWord = "</w>"
-        
+          
     /// A single morphological analysis of a word, as represented by a morpheme-delimited string of underlying lexical morphemes.
     public let underlyingForm: String
     
@@ -19,7 +16,6 @@ public struct MorphologicalAnalysis: Codable {
      */
     public init(_ underlyingForm: String, withIntermediateForm intermediateForm: String?, delimiter: String) {
         self.underlyingForm = underlyingForm
-        //self.morphemes = [MorphologicalAnalysis.startOfWord] + underlyingForm.components(separatedBy: delimiter) + [MorphologicalAnalysis.endOfWord]
         self.morphemes = underlyingForm.replacingOccurrences(of: "=", with: delimiter).replacingOccurrences(of: delimiter, with: " ")
         self.intermediateForm = intermediateForm
     }
